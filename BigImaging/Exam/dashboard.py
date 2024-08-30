8b6286ed50e167d6221d3a3a8ae77646abd17e0b
andypalmi <andypalmi@libero.it>
2024-08-30 11:26:57 +0200

    optimized it

--- BigImaging/Exam/dashboard.py
+++ BigImaging/Exam/dashboard.py
@@ -1,21 +1,25 @@
 import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output, State
+from dash import dcc, html
+from dash.dependencies import Input, Output
 import base64
 import cv2
 import numpy as np
 import torch
 import os
 import pandas as pd
-import torch
 from torchvision.transforms import transforms
-from utils.evaluate import load_model_from_checkpoint, evaluate_model
+from utils.evaluate import load_model_from_checkpoint
 from utils.utils import read_class_colors
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 # Set the current working directory to the directory where main.py is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
-print("Current working directory set to:", os.getcwd())
+logger.info("Current working directory set to: %s", os.getcwd())
 
 app = dash.Dash(__name__)
 
@@ -34,6 +38,18 @@ app.layout = html.Div([
 ])
 
 def parse_contents(contents, tiles_dim=1000, final_dim=256):
+    """
+    Parses the uploaded image contents and splits it into tiles.
+
+    Args:
+        contents (str): The base64 encoded contents of the uploaded image.
+        tiles_dim (int): The dimension of the tiles.
+        final_dim (int): The final dimension of the resized tiles.
+
+    Returns:
+        list: A list of image tiles.
+        tuple: The new shape of the image.
+    """
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
     img = cv2.imdecode(np.frombuffer(decoded, np.uint8), cv2.IMREAD_COLOR)
@@ -42,10 +58,9 @@ def parse_contents(contents, tiles_dim=1000, final_dim=256):
     # Calculate the new dimensions
     new_height = (img_height // tiles_dim) * tiles_dim
     new_width = (img_width // tiles_dim) * tiles_dim
-
     new_shape = (new_height, new_width)
 
-    print(f'\nold size {img_width} {img_height}, \nnew size: ({new_width}, {new_height})\n')
+    logger.info('Old size: (%d, %d), New size: (%d, %d)', img_width, img_height, new_width, new_height)
 
     # Resize the image
     img = cv2.resize(img, (new_width, new_height))
@@ -60,29 +75,39 @@ def parse_contents(contents, tiles_dim=1000, final_dim=256):
     return tiles, new_shape
 
 def get_model_options(models_folder):
+    """
+    Retrieves the available model options from the specified folder.
+
+    Args:
+        models_folder (str): The folder containing the model files.
+
+    Returns:
+        list: A list of dictionaries containing model labels and values.
+    """
     model_options = []
     for filename in os.listdir(models_folder):
         if filename.endswith('.pt') and '1000' in filename:
             parts = filename.split('_')
-            print(f'parts: {parts}')
+            logger.info('Parts: %s', parts)
             if 'mobilenet_v2' in filename:
                 arch = parts[-5]
                 encoder_name = '_'.join(parts[-2:]).split('.')[0]
             else:
                 arch = parts[-4]
                 encoder_name = parts[-1].split('.')[0]
-            print(f'encoder_name: {encoder_name}')
+            logger.info('Encoder name: %s', encoder_name)
             tiles_dim = parts[8]
             model_options.append({'label': '_'.join([arch, encoder_name]) + ' (Dim: ' + tiles_dim + ')', 'value': filename})
     return model_options
 
-
 def prepare_tiles(tiles, device='cuda' if torch.cuda.is_available() else 'cpu'):
     """
     Preprocesses a list of image tiles for input to a PyTorch model.
+
     Args:
         tiles (list): A list of image tiles.
         device (str, optional): The device to use for tensor operations. Defaults to 'cuda' if available, else 'cpu'.
+
     Returns:
         torch.Tensor: A batch tensor containing the preprocessed image tiles.
     """
@@ -92,21 +117,23 @@ def prepare_tiles(tiles, device='cuda' if torch.cuda.is_available() else 'cpu'):
     tiles_np = np.stack(tiles, axis=0)
     
     # Convert to tensor and move to device
-    batch_tensor = torch.tensor(tiles_np).permute(0, 3, 1, 2).float().to(device) / 255.0  # Reshape for PyTorch
+    batch_tensor = torch.tensor(tiles_np).permute(0, 3, 1, 2).float() / 255.0  # Reshape for PyTorch
     
-    # Apply normalization on the GPU
-    batch_tensor = normalize(batch_tensor)
+    # Apply normalization
+    batch_tensor = normalize(batch_tensor).to(device)
     
     return batch_tensor
 
 def reconstruct_mask(mask_tiles, img_shape, tiles_dim, final_dim=256):
     """
     Reconstructs a mask from a set of mask tiles.
+
     Args:
         mask_tiles (ndarray): Array of mask tiles.
         img_shape (tuple): Shape of the original image.
         tiles_dim (int): Dimension of the tiles.
         final_dim (int, optional): Dimension of the final mask tiles. Defaults to 256.
+
     Returns:
         ndarray: Reconstructed mask.
     """
@@ -115,18 +142,18 @@ def reconstruct_mask(mask_tiles, img_shape, tiles_dim, final_dim=256):
     reconstructed_mask = np.zeros((mask_width, mask_height), dtype=np.uint8)
     tile_index = 0
     num_tiles = mask_tiles.shape[0]
-    print(f'Number of tiles: {num_tiles}')
-    print(f'Image shape: {img_shape}\nMask shape: {(mask_width, mask_height)}')
+    logger.info('Number of tiles: %d', num_tiles)
+    logger.info('Image shape: %s, Mask shape: (%d, %d)', img_shape, mask_width, mask_height)
     
     for i in range(0, mask_width, final_dim):
         for j in range(0, mask_height, final_dim):
             if tile_index >= num_tiles:
-                print(f'Error: tile_index {tile_index} out of bounds for mask_tiles with size {num_tiles}')
+                logger.error('Error: tile_index %d out of bounds for mask_tiles with size %d', tile_index, num_tiles)
                 break
             reconstructed_mask[i:i + final_dim, j:j + final_dim] = mask_tiles[tile_index]
             tile_index += 1
     
-    print(f'Reconstructed mask shape: {reconstructed_mask.shape}')
+    logger.info('Reconstructed mask shape: %s', reconstructed_mask.shape)
     return reconstructed_mask
 
 class_colors = {
@@ -144,15 +171,15 @@ class_colors = {
 
 labels_colors, colors, num_classes = read_class_colors('data/ColorMasks/ColorPalette-Values.csv')
 
-def colorize_mask(predicted_mask):
+def colorize_mask(predicted_mask): 
     """
     Colorizes a predicted mask by assigning colors to different class IDs.
 
-    Parameters:
-    - predicted_mask (numpy.ndarray): The predicted mask to be colorized.
+    Args:
+        predicted_mask (numpy.ndarray): The predicted mask to be colorized.
 
     Returns:
-    - colorized_mask (numpy.ndarray): The colorized mask with assigned colors for each class ID.
+        numpy.ndarray: The colorized mask with assigned colors for each class ID.
     """
     colorized_mask = np.zeros((predicted_mask.shape[0], predicted_mask.shape[1], 3), dtype=np.uint8)
     for class_id, color in class_colors.items():
@@ -176,20 +203,24 @@ def update_dropdown_options(contents):
 )
 def update_output(contents, model_filename):
     if contents and model_filename:
-        tiles_dim = int(model_filename.split('_')[8])
-        tiles, new_shape = parse_contents(contents=contents, tiles_dim=tiles_dim)
-        print(f'Loaded {len(tiles)} tiles')
-        model_path = os.path.join('models', 'best', model_filename)
-        print(f'Loading model')
-        model, _, _, _ = load_model_from_checkpoint(model_path)
-        batch_tiles = prepare_tiles(tiles)
-        with torch.no_grad():
-            predicted_masks = model(batch_tiles)
-            predicted_masks = torch.softmax(predicted_masks, dim=1).argmax(dim=1).cpu().numpy()
-        predicted_mask = reconstruct_mask(predicted_masks, new_shape, tiles_dim, final_dim=256)
-        colorized_mask = colorize_mask(predicted_mask)
-        # Display the colorized mask
-        return html.Img(src=f'data:image/png;base64,{base64.b64encode(cv2.imencode(".png", colorized_mask)[1]).decode()}')
+        try:
+            tiles_dim = int(model_filename.split('_')[8])
+            tiles, new_shape = parse_contents(contents=contents, tiles_dim=tiles_dim)
+            logger.info('Loaded %d tiles', len(tiles))
+            model_path = os.path.join('models', 'best', model_filename)
+            logger.info('Loading model from %s', model_path)
+            model, _, _, _ = load_model_from_checkpoint(model_path)
+            batch_tiles = prepare_tiles(tiles)
+            with torch.no_grad():
+                predicted_masks = model(batch_tiles)
+                predicted_masks = torch.softmax(predicted_masks, dim=1).argmax(dim=1).cpu().numpy()
+            predicted_mask = reconstruct_mask(predicted_masks, new_shape, tiles_dim, final_dim=256)
+            colorized_mask = colorize_mask(predicted_mask)
+            # Display the colorized mask
+            return html.Img(src=f'data:image/png;base64,{base64.b64encode(cv2.imencode(".png", colorized_mask)[1]).decode()}')
+        except Exception as e:
+            logger.error('Error in update_output: %s', str(e))
+            return 'An error occurred during processing.'
     return 'Upload an image and select a model.'
 
 if __name__ == '__main__':
