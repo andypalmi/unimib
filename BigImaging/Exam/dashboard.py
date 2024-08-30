320cd5f457b92d4a80d8b886210d65e7ab245c5c
andypalmi <andypalmi@libero.it>
2024-08-30 12:07:17 +0200

    not working prototype

--- BigImaging/Exam/dashboard.py
+++ BigImaging/Exam/dashboard.py
@@ -1,6 +1,6 @@
 import dash
 from dash import dcc, html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import base64
 import cv2
 import numpy as np
@@ -11,6 +11,7 @@ from torchvision.transforms import transforms
 from utils.evaluate import load_model_from_checkpoint
 from utils.utils import read_class_colors
 import logging
+import hashlib
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -53,35 +54,32 @@ app.layout = html.Div(id='app-container', children=[
         ),
         style={'width': '50%', 'margin': '20px auto'}
     ),
-    html.Div(id='output-image-upload', style={'text-align': 'center'}),
+    dcc.Loading(
+        id="loading",
+        type="default",
+        children=html.Div(
+            id='output-image-upload', 
+            style={'text-align': 'center'})
+    ),
+    html.Div(id='hover-info', style={'position': 'absolute', 'pointer-events': 'none', 'background': 'white', 'padding': '5px', 'border': '1px solid black'}),
+    html.Div(id='mask-data', style={'display': 'none'})  # Hidden div to store mask data
 ])
 
+# Global dictionary to store predictions
+predictions_cache = {}
+
 def parse_contents(contents, tiles_dim=1000, final_dim=256):
-    """
-    Parses the uploaded image contents and splits it into tiles.
-
-    Args:
-        contents (str): The base64 encoded contents of the uploaded image.
-        tiles_dim (int): The dimension of the tiles.
-        final_dim (int): The final dimension of the resized tiles.
-
-    Returns:
-        list: A list of image tiles.
-        tuple: The new shape of the image.
-    """
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
     img = cv2.imdecode(np.frombuffer(decoded, np.uint8), cv2.IMREAD_COLOR)
     img_height, img_width, _ = img.shape
 
-    # Calculate the new dimensions
     new_height = (img_height // tiles_dim) * tiles_dim
     new_width = (img_width // tiles_dim) * tiles_dim
     new_shape = (new_height, new_width)
 
     logger.info('Old size: (%d, %d), New size: (%d, %d)', img_width, img_height, new_width, new_height)
 
-    # Resize the image
     img = cv2.resize(img, (new_width, new_height))
 
     tiles = []
@@ -94,15 +92,6 @@ def parse_contents(contents, tiles_dim=1000, final_dim=256):
     return tiles, new_shape, img
 
 def get_model_options(models_folder):
-    """
-    Retrieves the available model options from the specified folder.
-
-    Args:
-        models_folder (str): The folder containing the model files.
-
-    Returns:
-        list: A list of dictionaries containing model labels and values.
-    """
     model_options = []
     for filename in os.listdir(models_folder):
         if filename.endswith('.pt') and '1000' in filename:
@@ -120,42 +109,17 @@ def get_model_options(models_folder):
     return model_options
 
 def prepare_tiles(tiles, device='cuda' if torch.cuda.is_available() else 'cpu'):
-    """
-    Preprocesses a list of image tiles for input to a PyTorch model.
-
-    Args:
-        tiles (list): A list of image tiles.
-        device (str, optional): The device to use for tensor operations. Defaults to 'cuda' if available, else 'cpu'.
-
-    Returns:
-        torch.Tensor: A batch tensor containing the preprocessed image tiles.
-    """
     normalize = transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     
-    # Convert list of tiles to a single NumPy array
     tiles_np = np.stack(tiles, axis=0)
     
-    # Convert to tensor and move to device
-    batch_tensor = torch.tensor(tiles_np).permute(0, 3, 1, 2).float() / 255.0  # Reshape for PyTorch
+    batch_tensor = torch.tensor(tiles_np).permute(0, 3, 1, 2).float() / 255.0
     
-    # Apply normalization
     batch_tensor = normalize(batch_tensor).to(device)
     
     return batch_tensor
 
 def reconstruct_mask(mask_tiles, img_shape, tiles_dim, final_dim=256):
-    """
-    Reconstructs a mask from a set of mask tiles.
-
-    Args:
-        mask_tiles (ndarray): Array of mask tiles.
-        img_shape (tuple): Shape of the original image.
-        tiles_dim (int): Dimension of the tiles.
-        final_dim (int, optional): Dimension of the final mask tiles. Defaults to 256.
-
-    Returns:
-        ndarray: Reconstructed mask.
-    """
     mask_width = img_shape[0] // tiles_dim * final_dim
     mask_height = img_shape[1] // tiles_dim * final_dim
     reconstructed_mask = np.zeros((mask_width, mask_height), dtype=np.uint8)
@@ -191,32 +155,12 @@ class_colors = {
 labels_colors, colors, num_classes = read_class_colors('data/ColorMasks/ColorPalette-Values.csv')
 
 def colorize_mask(predicted_mask):
-    """
-    Colorizes a predicted mask by assigning colors to different class IDs.
-
-    Args:
-        predicted_mask (numpy.ndarray): The predicted mask to be colorized.
-
-    Returns:
-        numpy.ndarray: The colorized mask with assigned colors for each class ID.
-    """
     colorized_mask = np.zeros((predicted_mask.shape[0], predicted_mask.shape[1], 3), dtype=np.uint8)
     for class_id, color in class_colors.items():
         colorized_mask[predicted_mask == class_id] = color
     return colorized_mask
 
 def overlay_mask_on_image(image, mask, alpha=0.5):
-    """
-    Overlays the colorized mask on the original image with adjustable alpha transparency.
-
-    Args:
-        image (numpy.ndarray): The original image.
-        mask (numpy.ndarray): The colorized mask.
-        alpha (float): The alpha transparency for the mask.
-
-    Returns:
-        numpy.ndarray: The image with the mask overlay.
-    """
     overlay = cv2.addWeighted(image, 1 - alpha, mask, alpha, 0)
     return overlay
 
@@ -232,6 +176,7 @@ def update_dropdown_options(contents):
 
 @app.callback(
     Output('output-image-upload', 'children'),
+    Output('mask-data', 'children'),  # Output to store mask data
     Input('upload-image', 'contents'),
     Input('model-dropdown', 'value'),
     Input('alpha-slider', 'value')
@@ -239,26 +184,80 @@ def update_dropdown_options(contents):
 def update_output(contents, model_filename, alpha):
     if contents and model_filename:
         try:
-            tiles_dim = int(model_filename.split('_')[8])
-            tiles, new_shape, original_image = parse_contents(contents=contents, tiles_dim=tiles_dim)
-            logger.info('Loaded %d tiles', len(tiles))
-            model_path = os.path.join('models', 'best', model_filename)
-            logger.info('Loading model from %s', model_path)
-            model, _, _, _ = load_model_from_checkpoint(model_path)
-            batch_tiles = prepare_tiles(tiles)
-            with torch.no_grad():
-                predicted_masks = model(batch_tiles)
-                predicted_masks = torch.softmax(predicted_masks, dim=1).argmax(dim=1).cpu().numpy()
-            predicted_mask = reconstruct_mask(predicted_masks, new_shape, tiles_dim, final_dim=256)
-            colorized_mask = colorize_mask(predicted_mask)
-            resized_original_image = cv2.resize(original_image, (colorized_mask.shape[1], colorized_mask.shape[0]))
+            # Generate a hash for the uploaded content
+            content_hash = hashlib.md5(contents.encode()).hexdigest()
+            
+            if content_hash in predictions_cache:
+                logger.info('Using cached prediction')
+                colorized_mask, resized_original_image = predictions_cache[content_hash]
+            else:
+                tiles_dim = int(model_filename.split('_')[8])
+                tiles, new_shape, original_image = parse_contents(contents=contents, tiles_dim=tiles_dim)
+                logger.info('Loaded %d tiles', len(tiles))
+                model_path = os.path.join('models', 'best', model_filename)
+                logger.info('Loading model from %s', model_path)
+                model, _, _, _ = load_model_from_checkpoint(model_path)
+                batch_tiles = prepare_tiles(tiles)
+                with torch.no_grad():
+                    predicted_masks = model(batch_tiles)
+                    predicted_masks = torch.softmax(predicted_masks, dim=1).argmax(dim=1).cpu().numpy()
+                predicted_mask = reconstruct_mask(predicted_masks, new_shape, tiles_dim, final_dim=256)
+                colorized_mask = colorize_mask(predicted_mask)
+                resized_original_image = cv2.resize(original_image, (colorized_mask.shape[1], colorized_mask.shape[0]))
+                
+                # Cache the prediction
+                predictions_cache[content_hash] = (colorized_mask, resized_original_image)
+            
             overlay_image = overlay_mask_on_image(resized_original_image, colorized_mask, alpha)
+            # Encode the mask as a base64 string
+            _, buffer = cv2.imencode('.png', predicted_mask)
+            mask_data = base64.b64encode(buffer).decode('utf-8')
             # Display the overlay image
-            return html.Img(src=f'data:image/png;base64,{base64.b64encode(cv2.imencode(".png", overlay_image)[1]).decode()}')
+            return html.Div([
+                html.Img(id='overlay-image', src=f'data:image/png;base64,{base64.b64encode(cv2.imencode(".png", overlay_image)[1]).decode()}'),
+                dcc.Tooltip(id='tooltip')
+            ]), mask_data
         except Exception as e:
             logger.error('Error in update_output: %s', str(e))
-            return 'An error occurred during processing.'
-    return 'Upload an image and select a model.'
+            return 'An error occurred during processing.', ''
+    return 'Upload an image and select a model.', ''
+
+# JavaScript for cursor tracking and tooltip
+app.clientside_callback(
+    """
+    function(imageData, maskData) {
+        if (!imageData || !maskData) return;
+        const img = document.getElementById('overlay-image');
+        const tooltip = document.getElementById('tooltip');
+        const mask = new Image();
+        mask.src = 'data:image/png;base64,' + maskData;
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        mask.onload = function() {
+            canvas.width = mask.width;
+            canvas.height = mask.height;
+            ctx.drawImage(mask, 0, 0);
+        };
+        img.addEventListener('mousemove', function(event) {
+            const rect = img.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+            const pixel = ctx.getImageData(x, y, 1, 1).data;
+            const classId = pixel[0]; // Assuming class ID is stored in the red channel
+            tooltip.style.left = `${event.clientX + 10}px`;
+            tooltip.style.top = `${event.clientY + 10}px`;
+            tooltip.innerHTML = `Class: ${classId}`;
+            tooltip.style.display = 'block';
+        });
+        img.addEventListener('mouseout', function() {
+            tooltip.style.display = 'none';
+        });
+    }
+    """,
+    Output('hover-info', 'children'),
+    Input('output-image-upload', 'children'),
+    State('mask-data', 'children')
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
